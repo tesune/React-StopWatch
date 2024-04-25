7d6cb4cb90092878a87102f77df42d27d16a1567
ZheHeang <tesune0316@gmail.com>
2024-04-25 22:00:26 +0800

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/stopwatch-2024/",
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
+  // server: {
+  //   port: 3000,
+  // },
 })
